a741faf08050bfc14a20a6b1b49e1380e4490d03
Leczok12 <kamilleczo@op.pl>
2025-08-05 17:58:13 +0200

    inactive sensor detection

--- src/actions/glucose-state.ts
+++ src/actions/glucose-state.ts
@@ -21,6 +21,8 @@ type Settings = {
     error: string | undefined;
 };
 
+type ExtendedLibreCgmData = LibreCgmData & { isActive: boolean };
+
 type AcctionInterface = WillAppearEvent<Settings> | KeyDownEvent<Settings> | DidReceiveSettingsEvent<Settings>;
 
 @action({ UUID: 'com.kamil-leczkowski.librelink-plugin.glucose-state' })
@@ -48,16 +50,22 @@ export class GlucoseState extends SingletonAction<Settings> {
         }
     };
 
-    private readLibreLinkUpClientData = async () => {
+    private readLibreLinkUpClientData = async (): Promise<ExtendedLibreCgmData | undefined> => {
         if (this.libreLinkUpClient === undefined) return undefined;
         const rawData = await this.libreLinkUpClient.readRaw();
-        const data = (await this.libreLinkUpClient.read()).current;
+        const cgmData = (await this.libreLinkUpClient.read()).current;
 
-        data.isHigh = data.value >= rawData.connection.targetHigh;
-        data.isLow = data.value <= rawData.connection.targetLow;
+        const extendedCgmData: ExtendedLibreCgmData = {
+            ...cgmData,
+            isHigh: cgmData.value >= rawData.connection.targetHigh,
+            isLow: cgmData.value >= rawData.connection.targetLow,
+            isActive:
+                Math.floor(Date.now() / 1000) - rawData.connection.sensor.a > 3600 &&
+                Math.floor(Date.now() / 1000) - rawData.connection.sensor.a < 1209600
+        };
 
-        streamDeck.logger.info('cgm data -> ' + JSON.stringify(data));
-        return data;
+        streamDeck.logger.info('cgm data -> ' + JSON.stringify(rawData));
+        return extendedCgmData;
     };
 
     private renderLibreLinkUpClientData = async () => {
@@ -70,8 +78,15 @@ export class GlucoseState extends SingletonAction<Settings> {
                 return;
             }
 
+            if (data === undefined) {
+                action.setImage('');
+                action.setTitle('');
+                return;
+            }
+
             const color = (() => {
-                if (data.isHigh) return '#fc9c02';
+                if (!data.isActive) return '#666666';
+                else if (data.isHigh) return '#fc9c02';
                 else if (data.isLow) return '#ff0000';
                 else return '#00ff00';
             })();
@@ -95,14 +110,18 @@ export class GlucoseState extends SingletonAction<Settings> {
 
             const svg = `
             <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144 144" width="144" height="144">
-                <path id="Kształt 1" fill-rule="evenodd" fill="${color}" d="m144 97v47h-144v-47z"/>
-                <path fill-rule="evenodd" fill="#ffffff" transform="rotate(${angle} 69 118.3)" stroke="#ffffff" stroke-width="5" d="m54 120.3q0-0.2 0.1-0.5 0.1-0.2 0.3-0.4 0.1-0.2 0.4-0.3 0.2 0 0.5 0h29.6l-7.9-8c-0.2-0.2-0.4-0.5-0.4-0.8 0-0.4 0.2-0.7 0.4-0.9 0.2-0.3 0.5-0.4 0.9-0.4 0.3 0 0.6 0.1 0.9 0.4l10 10q0.2 0.2 0.3 0.4 0.1 0.3 0.1 0.5 0 0.3-0.1 0.5-0.1 0.2-0.3 0.4l-10.1 10.1c-0.2 0.2-0.5 0.3-0.8 0.3-0.4 0-0.7-0.1-0.9-0.3-0.3-0.3-0.4-0.6-0.4-0.9 0-0.4 0.1-0.7 0.4-0.9l7.9-7.9h-29.6q-0.3 0-0.5-0.1-0.3-0.1-0.4-0.3-0.2-0.2-0.3-0.4-0.1-0.2-0.1-0.5z"/>
+                <path fill-rule="evenodd" fill="${color}" d="m144 97v47h-144v-47z"/>
+                ${
+                    data.isActive
+                        ? `<path fill-rule="evenodd" fill="#ffffff" transform="rotate(${angle} 69 118.3)" stroke="#ffffff" stroke-width="5" d="m54 120.3q0-0.2 0.1-0.5 0.1-0.2 0.3-0.4 0.1-0.2 0.4-0.3 0.2 0 0.5 0h29.6l-7.9-8c-0.2-0.2-0.4-0.5-0.4-0.8 0-0.4 0.2-0.7 0.4-0.9 0.2-0.3 0.5-0.4 0.9-0.4 0.3 0 0.6 0.1 0.9 0.4l10 10q0.2 0.2 0.3 0.4 0.1 0.3 0.1 0.5 0 0.3-0.1 0.5-0.1 0.2-0.3 0.4l-10.1 10.1c-0.2 0.2-0.5 0.3-0.8 0.3-0.4 0-0.7-0.1-0.9-0.3-0.3-0.3-0.4-0.6-0.4-0.9 0-0.4 0.1-0.7 0.4-0.9l7.9-7.9h-29.6q-0.3 0-0.5-0.1-0.3-0.1-0.4-0.3-0.2-0.2-0.3-0.4-0.1-0.2-0.1-0.5z"/>`
+                        : ''
+                }
             </svg>`;
 
             const base64svg = btoa(unescape(encodeURIComponent(svg)));
 
             action.setImage(`data:image/svg+xml;base64,${base64svg}`);
-            action.setTitle(data.value.toString());
+            action.setTitle(data.isActive ? data.value.toString() : '---');
         }
     };
 
